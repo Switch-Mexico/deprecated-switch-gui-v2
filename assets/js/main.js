d97be9ad467a9be6533ac91d72972d10b4f0ca45
alexter42 <lexie@protonmail.ch>
2017-12-04 19:22:22 -0600

    Update landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,127 +1,156 @@
-"use strict";
+$(document).ready(function ($) {
+    'use strict';
 
 
-jQuery(document).ready(function ($) {
-
-	$(window).load(function () {
-		$(".loaded").fadeOut();
-		$(".preloader").delay(1000).fadeOut("slow");
-	});
-    /*---------------------------------------------*
-     * Mobile menu
-     ---------------------------------------------*/
-    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: (target.offset().top - 40)
-                }, 1000);
-                if ($('.navbar-toggle').css('display') != 'none') {
-                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
-                }
-                return false;
-            }
-        }
+/* ---------------------------------------------
+     page  Prealoader
+ --------------------------------------------- */
+    $(window).on('load', function () {
+        $("#loading-center-page").fadeOut();
+        $("#loading-page").delay(400).fadeOut("slow");
     });
 
 
-
-    /*---------------------------------------------*
-     * STICKY scroll
-     ---------------------------------------------*/
-
-    $.localScroll();
-
-
-
-    /*---------------------------------------------*
-     * Counter 
-     ---------------------------------------------*/
-
-    $('.statistic-counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-
-
-
-    /*---------------------------------------------*
-     * WOW
-     ---------------------------------------------*/
-
-        var wow = new WOW({
-            mobile: false // trigger animations on mobile devices (default is true)
-        });
-        wow.init();
-
-
-    /* ---------------------------------------------------------------------
-     Carousel
-     ---------------------------------------------------------------------= */
-
-    $('.slider').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-       dots: true,
-	   nav:false,
-	   navText: [
-                    "<i class='lnr lnr-chevron-left'></i>",
-                    "<i class='lnr lnr-chevron-right'></i>"
-                ],
-      autoplayHoverPause: true
-
-    });
-
-    
-
-	
-	$('.chart').easyPieChart({
-		animate: 2000,
-            scaleColor: false,
-            lineWidth: 10,
-            lineCap: 'square',
-            size: 130,
-            trackColor: false,
-            barColor: '#498af3',
-            onStep: function (from, to, percent) {
-                $(this.el).find('.percent').text(Math.round(percent));
-            }
-	});
-	
-
-// main-menu-scroll
-
-	jQuery(window).scroll(function () {
-	  var top = jQuery(document).scrollTop();
-		var height = 300;
-	  //alert(batas);
-	  
-	  if (top > height) {
-		jQuery('.navbar-fixed-top').addClass('menu-scroll');
-	  } else {
-	   jQuery('.navbar-fixed-top').removeClass('menu-scroll');
-	  }
-	});	 
-	
-// scroll Up
-
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 600) {
-            $('.scrollup').fadeIn('slow');
-        } else {
-            $('.scrollup').fadeOut('slow');
-        }
-    });
-    $('.scrollup').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 1000);
-        return false;
-    });	
-
-    //End
 });
+
+/* ---------------------------------------------
+ owl caroussel
+ --------------------------------------------- */
+
+$('.screenshot_slider').owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    nav: true,
+    margin: 1,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    items: 3,
+    smartSpeed: 500,
+    autoWidth:true,
+    center: true,
+    navText: ['<span class="icon-arrow-left"></span>', '<span class="icon-arrow-right"></span>'],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 3
+        },
+        1200: {
+            items: 3,
+            
+        }
+    }
+});
+
+
+$('.testimonial-caroussel').owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 500,
+    center: true,
+    navText: ['<span class="icon-arrow-left"></span>', '<span class="icon-arrow-right"></span>'],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1
+
+        },
+        1200: {
+            items: 1
+        }
+    }
+});
+
+
+/*--------------------
+ MAGNIFIC POPUP JS
+ ----------------------*/
+$('.popup-image').magnificPopup({
+    type: 'image',
+    removalDelay: 300,
+    mainClass: 'mfp-with-zoom',
+    gallery: {
+        enabled: true
+    },
+    zoom: {
+        enabled: true,
+
+        duration: 300,
+        easing: 'ease-in-out',
+
+        opener: function (openerElement) {
+
+            return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+    }
+});
+
+/* ---------------------------------------------
+ Back top page scroll up
+ --------------------------------------------- */
+
+
+$.scrollUp({
+    scrollText: '<i class="icon-arrow-up"></i>',
+    easingType: 'linear',
+    scrollSpeed: 900,
+    animation: 'fade'
+});
+
+
+/* ---------------------------------------------
+ WoW plugin
+ --------------------------------------------- */
+
+new WOW().init({
+    mobile: true,
+});
+
+/* ---------------------------------------------
+ Smooth scroll
+ --------------------------------------------- */
+
+  $('a.section-scroll[href*="#"]:not([href="#"])').on('click', function (event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 750);
+            return false;
+        }
+    }
+});
+
+
+/*----------------------------------------
+ Newsletter Subscribe
+ --------------------------------------*/
+
+
+    $(".subscribe-mail").ajaxChimp({
+        callback: mailchimpCallRep,
+        url: "mailchimp-post-url" //Replace this with your own mailchimp post URL. Just paste the url inside "".
+    });
+
+function mailchimpCallRep(resp)
+{
+    if (resp.result === "success") {
+        $(".sucess-message").html(resp.msg).fadeIn(1000);
+        $(".error-message").fadeOut(500);
+    } else if (resp.result === "error") {
+        $(".error-message").html(resp.msg).fadeIn(1000);
+    }
+}
+
